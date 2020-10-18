bc45e0470ac65c9ac680b8450ed0358929c42fe1
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-10-19 00:46:41 +0530

    second commit

---
+++ Simplego/operators.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 30
+	var b int = 5
+	fmt.Printf("Kloud - Go Assignment Operators")
+	a += b
+	fmt.Printf("a+=b :%d\n", a)
+	a -= b
+	fmt.Printf("a-=b :%d\n, a")
+	a *= b
+	fmt.Printf("a*=b :%d\n, a")
+	a /= b
+	fmt.Printf("a/=b :%d\n", a)
+
+}
