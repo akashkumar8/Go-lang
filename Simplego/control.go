91c0f9c1d5bd8b8fb386f1c25d0df8de10bcb6a2
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-10-19 16:29:54 +0530

    third commit

---
+++ Simplego/control.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a int = 10
+
+	if a%2 == 0 {
+
+		fmt.Printf("a is even\n")
+	} else {
+
+		fmt.Printf("a is odd\n")
+	}
+	fmt.Printf("value of a is : %d\n", a)
+}
