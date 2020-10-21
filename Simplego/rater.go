25181b446395c8dcdfbf66db7736dc7daf063440
Akash Kumar <67784046+akashkumar8@users.noreply.github.com>
2020-10-21 16:50:39 +0530

    Update rater.go

--- Simplego/rater.go
+++ Simplego/rater.go
@@ -16,5 +16,14 @@ func main() {
 	reader := bufio.NewReader(os.stdin)
 	fmt.Println("Please enter your full name")
 	name, _ := reader.ReadString(\n)
+	
+	
+	reader = bufio.NewReader(os.stdin)
+	fmt.Println("Please rate our Kloud learning Officials between 1 and 5: ")
+	userRating, _ = reader.ReadString('\n')
+	mynumRating, _ := strconv.ParseFloat(strings.TrimSpace(userRating), 64)
+
+
+	fmt.Printf(%v, %v, name,mynumRating)
 
 }
